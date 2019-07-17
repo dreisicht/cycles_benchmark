961c933c31ea82c94a46937920434e0216f0e24e
Peter Baintner <peterbaintner@gmail.com>
2019-07-17 18:53:46 +0200

    update

--- start.py
+++ start.py
@@ -4,18 +4,29 @@ import time
 
 print("Preparing Benchmark.")
 
+def render(version, filename, processor):
+    base_string = "{} -b ./blends/{}.blend -P {} -o ./img/{} -f {}"
+    
+    command_str_tmp = base_string.format(version, filename[0], filename[0], processor, filename[1])
+    command_str = command_str_tmp.replace("./", cwd)
+    print("||||||||||||||||||||||||||||||||||||||||||||||", command_str)
+    subprocess.Popen(command_str)
+
+cwd = os.getcwd().replace("\\", "/") +"/"
+
 blender27 = "./blender/blender-2.79b/blender.exe"
 blender28 = "./blender/blender-2.80rc1/blender.exe"
 
 cpu = "./scripts/cpu.py"
 gpu = "./scripts/gpu.py"
 
-attic = "attic_2.8"
-bmw = "bmw_2.7"
-classroom = "classroom_2.7"
-cube27 = "default_cube_2.7"
-cube28 = "default_cube_2.8"
+attic = ("attic_2.8", 1)
+bmw = ("bmw_2.7", 1)
+classroom = ("classroom_2.7", 1)
+cube27 = ("default_cube_2.7", 1)
+cube28 = ("default_cube_2.8", 1)
 
-base_string = "{} -b ./blends/{}.blend -P {} -o ./img/ -a"
+render(blender27, cube27, cpu)
 
-subprocess.Popen(base_string.format(blender27, cube27, cpu))
+
+
